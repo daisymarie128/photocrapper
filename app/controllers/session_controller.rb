639cfa029ffa6a8a2a4f231520a074ccba31bb0d
Daisy Smith <daisy_madonna808@Daisy-Smiths-MacBook-Pro.local>
2014-07-04 09:56:14 +1000

    i hope this is all

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,5 +1,9 @@
 class SessionController < ApplicationController
-    def new
+
+  before_action :check_if_logged_in, :except => [:new, :create, :follow, :unfollow]
+  before_action :check_if_admin, :only => [:index]
+
+  def new
   end
 
   def create
@@ -16,5 +20,19 @@ class SessionController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  private
+
+  #chech if the user is logged in. show only their stuff
+  #loads the smart navs
+  def check_if_logged_in
+    redirect_to(new_user_path) if @current_user.nil?
+  end
+
+  #loads the smart navs
+  #^^ should this be changed I dont fucking know
+  def check_if_admin
+    redirect_to(root_path) unless @current_user.is_admin?
   end
 end
